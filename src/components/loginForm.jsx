5f9f1813e4dd80ce2e659246d32315b9268dfdc2
erfan-najafloo <erfan.najafloo@gmail.com>
2025-09-21 20:37:44 +0430

    useState for email and password inputs added

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,28 +1,40 @@
+import { useState } from "react";
 
-export function LoginForm(){
+export function LoginForm() {
+    const [ email, setEmail]  = useState('');
+    const  [password, setPassword]  = useState('');
+
     return (
-      <>
-         <form action="" method="post">
-        <p class="title">Login Form</p>
-        <div>
-            <input type="text" placeholder="Email" className="email-input" />
-        </div>
-        <div>
-            <input type="password" placeholder="Password" className="password-input" />
-        </div>
-        <div>
-            <button className="login-button">
-                login
-            </button>
-        </div>
+        <>
+            <form action="" method="post">
+                <p class="title">Login Form</p>
+                <div>
+                    <input type="text" placeholder="Email" className="email-input" value={email} onChange={(event) => {
+                        setEmail(event.target.value);
+                       console.log(email);
+                    }} />
+                </div>
+                <div>
+                    <input type="password" placeholder="Password" className="password-input" value={password} onChange={(event) => {
+                        setPassword(event.target.value);
+                        console.log(password);
+                    }} />
+                </div>
+                <div>
+                    <button className="login-button">
+                        login
+                    </button>
+                </div>
 
 
-        <p className="error-message">
-        </p>
-        <p className="success-message">
-        </p>
-    </form>
-      </>
-         );}
-       
+                <p className="error-message">
+                </p>
+                <p className="success-message">
+                </p>
+            </form>
+        </>
     
+    );
+     
+}
+
