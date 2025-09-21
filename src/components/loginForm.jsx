3b22456b2222c80f76f1bcde95ad17be741a891b
erfan-najafloo <erfan.najafloo@gmail.com>
2025-09-21 20:12:44 +0430

    basic LoginForm JSX structure added

---
+++ src/components/loginForm.jsx
@@ -0,0 +1,27 @@
+export function LoginForm(){
+    return (
+      <>
+         <form action="" method="post">
+        <p class="title">Login Form</p>
+        <div>
+            <input type="text" placeholder="Email" className="email-input" />
+        </div>
+        <div>
+            <input type="password" placeholder="Password" className="password-input" />
+        </div>
+        <div>
+            <button className="login-button">
+                login
+            </button>
+        </div>
+
+
+        <p className="error-message">
+        </p>
+        <p className="success-message">
+        </p>
+    </form>
+      </>
+         );}
+       
+    
